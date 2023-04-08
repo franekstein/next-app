ae3e9b991f35fc1b13a32d65419f763b31f498ca
franekstein <damian.kieliszek@email.com>
2023-04-08 16:41:49 +0200

    update checkly tests

--- __checks__/404-page.check.js
+++ __checks__/404-page.check.js
@@ -8,11 +8,10 @@ import { expect, test } from '@playwright/test';
 test('visit 404 page and take screenshot', async ({ page }) => {
   // If available, we set the target URL to a preview deployment URL provided by the ENVIRONMENT_URL created by Vercel.
   // Otherwise, we use the Production URL.
-  const targetUrl =
-    process.env.ENVIRONMENT_URL || 'https://next-app-phi-virid.vercel.app';
+  const targetUrl = `${process.env.ENVIRONMENT_URL}/page-that-should-not-exist`;
 
   // We visit the page. This waits for the "load" event by default.
-  const response = await page.goto(`${targetUrl}/page-that-should-not-exist`);
+  const response = await page.goto(targetUrl);
 
   // Test that the response did fail
   expect(response?.status()).toBe(404);
@@ -22,5 +21,5 @@ test('visit 404 page and take screenshot', async ({ page }) => {
   );
 
   // Take a screenshot
-  await page.screenshot({ path: '404.jpg' });
+  await page.screenshot({ path: '404-page.jpg' });
 });
--- __checks__/main-page.check.js
+++ __checks__/main-page.check.js
@@ -5,11 +5,10 @@
  */
 import { expect, test } from '@playwright/test';
 
-test('visit page and take screenshot', async ({ page }) => {
+test('visit main page and take screenshot', async ({ page }) => {
   // If available, we set the target URL to a preview deployment URL provided by the ENVIRONMENT_URL created by Vercel.
   // Otherwise, we use the Production URL.
-  const targetUrl =
-    process.env.ENVIRONMENT_URL || 'https://next-app-phi-virid.vercel.app';
+  const targetUrl = `${process.env.ENVIRONMENT_URL}`;
 
   // We visit the page. This waits for the "load" event by default.
   const response = await page.goto(targetUrl);
@@ -18,5 +17,5 @@ test('visit page and take screenshot', async ({ page }) => {
   expect(response?.status()).toBeLessThan(400);
 
   // Take a screenshot
-  await page.screenshot({ path: 'screenshot.jpg' });
+  await page.screenshot({ path: 'main-page.jpg' });
 });
